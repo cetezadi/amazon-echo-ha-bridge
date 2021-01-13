ad81c2115754edd50ac62c28ceba5fe2236d33f6
cetezadi <cam_001@hotmail.com>
2021-01-12 16:56:14 -0800

    Fix SpringBoot tests

--- src/test/java/demo/DemoApplicationTests.java
+++ src/test/java/demo/DemoApplicationTests.java
@@ -3,13 +3,17 @@ package demo;
 import com.armzilla.ha.SpringbootEntry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringbootEntry.class)
+@SpringBootTest
+@EnableAutoConfiguration
+@ContextConfiguration(classes = SpringbootEntry.class)
 @TestPropertySource(locations="classpath:test.properties")
 @WebAppConfiguration
 public class DemoApplicationTests {
